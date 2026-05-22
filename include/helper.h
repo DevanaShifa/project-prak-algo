0ca1cb456ad0c8210a8173594af1b1e8c0ef836e
abcdavk <xloot.mcpedl@gmail.com>
2026-05-23 06:17:40 +0700

    create a rangeInput template

--- include/helper.h
+++ include/helper.h
@@ -9,7 +9,7 @@ using namespace std;
  * @param value variabel penyimpan input
  */
 template<typename T>
-void safeInput(string msg, T& value) {
+void safeInput(const string msg, T& value) {
     string input;
 
     while (true)
@@ -25,5 +25,42 @@ void safeInput(string msg, T& value) {
         }
 
         cout << "Tipedata tidak valid!\n";
+    }
+}
+
+/**
+ * memastikan input harus berada di range tertentu
+ * @param msg pesan output yang ditampilkan
+ * @param value variabel penyimpan input
+ * @param min nilai minimum
+ * @param max nilai maximum
+ */
+template<typename T>
+void rangedInput(
+    const string msg, 
+    T& value, 
+    int min,
+    int max
+) {
+    string input;
+
+    while (true)
+    {
+        cout << msg;
+
+        getline(cin, input);
+
+        if (input < min) {
+            cout << "Nilai harus lebih dari " << min;
+        } else if (input > max) {
+            cout << "Nilai harus kurang dari " << max;
+        } else {
+            stringstream ss(input);
+            if (ss >> value) {
+                return;
+            }
+    
+            cout << "Tipedata tidak valid!\n";
+        }
     }
 }
