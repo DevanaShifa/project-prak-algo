ee159f59c51a156b43a2fcd86e820ac076397697
abcdavk <xloot.mcpedl@gmail.com>
2026-05-23 06:58:34 +0700

    working on inputBarang

--- src/barang.cpp
+++ src/barang.cpp
@@ -20,8 +20,48 @@ void inputBarang() {
 
     safeInput<int>("Banyak data yang ingin diinput: ", jumlah);
 
-    for (int i = 0; i < jumlah; i++)
-    {
-        
+    for (int i = 0; i < jumlah; i++) {
+        int hari, bulan, tahun;
+        int jam, menit;
+        cout << "\nData ke-" << totalBarang + 1 << endl;
+
+        dataBarang[totalBarang].idBarang = totalBarang + 1;
+
+        cout << "Nama Barang          : "; cin.getline(dataBarang[totalBarang].namaBarang, 50);
+        cout << "Kategori             : "; cin.getline(dataBarang[totalBarang].kategori, 50);
+        cout << "Deskripsi            : "; cin.getline(dataBarang[totalBarang].deskripsi, 100);
+        cout << "Lokasi               : "; cin.getline(dataBarang[totalBarang].lokasi, 50);
+
+        cout << endl << "Tanggal: " << endl;
+        rangedInput<int>("- Hari (1-31)        : ", hari, 1, 31);
+        rangedInput<int>("- Bulan (1-12)       : ", bulan, 1, 12);
+        rangedInput<int>("- Tahun              : ", tahun, 2000, 9999);
+
+        snprintf(dataBarang[i].tanggal, 50,
+                "%d/%d/%d",
+                hari,
+                bulan,
+                tahun
+        );
+
+        overwriteAbove(4);
+        cout << "Tanggal              : " << dataBarang[i].tanggal << endl;
+
+        cout << "Waktu: " << endl;
+        rangedInput<int>("- Jam (0-23)         : ", jam, 0, 11);
+        rangedInput<int>("- Menit (0-59)       : ", menit, 0, 59);
+
+        snprintf(dataBarang[i].waktu, 50,
+                "%d:%d",
+                jam,
+                menit
+        );
+
+        overwriteAbove(3);
+        cout << "Waktu                : " << dataBarang[i].waktu << endl;
+
+        totalBarang++;
+
     }
+    cout << "\nData berhasil ditambahkan!\n";
 }
