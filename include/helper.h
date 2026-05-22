d380b005348505e7452eadf7147ca8dfa3e3c760
abcdavk <xloot.mcpedl@gmail.com>
2026-05-23 05:31:21 +0700

    create a safeInput template

--- include/helper.h
+++ include/helper.h
@@ -1,4 +1,29 @@
 #pragma once
 #include <iostream>
+#include <sstream>
+using namespace std;
 
-void inputInt(std::string msg, int& output);
+/**
+ * validasi input agar program tidak error ketika tipe data tidak sesuai
+ * @param msg pesan output yang ditampilkan
+ * @param value variabel penyimpan input
+ */
+template<typename T>
+void safeInput(string msg, T& value) {
+    string input;
+
+    while (true)
+    {
+        cout << msg;
+
+        getline(cin, input);
+
+        stringstream ss(input);
+
+        if (ss >> value) {
+            return;
+        }
+
+        cout << "Tipedata tidak valid!\n";
+    }
+}
