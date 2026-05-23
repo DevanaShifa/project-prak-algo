63a0c457dc2aefb18422f83088f677236977bcfb
abcdavk <xloot.mcpedl@gmail.com>
2026-05-23 08:18:47 +0700

    working on main menu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include "display.h"
 #include "barang.h"
+#include "helper.h"
+
+using namespace std;
 
 // debugging
 int main() {
-    inputBarang();
+    // inputBarang();
+
+    int menu;
+
+    do
+    {
+        CLEAR_SCREEN;
+        tampilData();
+        tampilHint();
+
+        safeInput<int>("> ", menu);
+
+        switch (menu)
+        {
+        case 1:
+            break;
+        case 2:
+            inputBarang();
+            break;
+        
+        default:
+            cout << "Menu tidak valid!" << endl;
+            cin.ignore();
+            cin.get();
+            break;
+        }
+    } while (menu != 6);
 }
