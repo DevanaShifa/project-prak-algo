f2344326848af4a37c6799a9e19917b9e0b9c0f8
abcdavk <xloot.mcpedl@gmail.com>
2026-05-23 08:18:28 +0700

    create tampilHint

--- src/display.cpp
+++ src/display.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
+#include <iomanip>
 #include "display.h"
 #include "barang.h"
 #include "helper.h"
 using namespace std;
 
-void tampilMenu() {
-    int menu;
-
-    do
-    {
-        switch (menu)
-        {
-        case 1:
-            break;
-        
-        default:
-            cout << "Menu tidak valid!" << endl;
-            cin.ignore();
-            cin.get();
-            break;
-        }
-    } while (menu != 6);
-    
+void tampilHint() {
+    cout << "Hint:" << endl;
+    cout << setfill(' ');
+    cout << left
+        << setw(30) << "[1] Pilih Barang"
+        << setw(30) << "[3] Cari Barang"
+        << setw(30) << "[5] Pengaturan"
+        << endl
+        << setw(30) << "[2] Buat Laporan"
+        << setw(30) << "[4] Sort Barang"
+        << setw(30) << "[0] Kembali/Keluar"
+        << endl;
+    cout << setfill('-') << setw(90) << "" << endl;
 }
 
+void tampilData() {
+    cout << setfill('-') << setw(90) << "" << endl;
+
+    cout << setfill(' ');
+    cout << left
+        << setw(4) << "ID"
+        << setw(18) << "NAMA BARANG"
+        << setw(18) << "KATEGORI"
+        << setw(18) << "LOKASI"
+        << setw(18) << "TANGGAL"
+        << setw(14) << "WAKTU"
+        << endl;
+    cout << setfill('-') << setw(90) << "" << endl;
+    cout << setfill(' ');
+
+    for (int i = 0; i < totalBarang; i++)
+    {
+        cout << left
+            << setw(4) << dataBarang[i].idBarang
+            << setw(18) << dataBarang[i].namaBarang
+            << setw(18) << dataBarang[i].kategori
+            << setw(18) << dataBarang[i].lokasi
+            << setw(18) << dataBarang[i].tanggal
+            << setw(14) << dataBarang[i].waktu
+            << endl;
+    }
+    cout << setfill('-') << setw(90) << "" << endl;
+}
