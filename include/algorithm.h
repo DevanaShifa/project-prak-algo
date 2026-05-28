c2c23a48825153b63fa6b7cb7d4e1b63d278ff39
abcdavk <xloot.mcpedl@gmail.com>
2026-05-28 15:30:58 +0700

    add bubbleSort algorithm

---
+++ include/algorithm.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/**
+ * @tparam T tipe data elemen array
+ * @tparam N ukuran array
+ * @tparam Compare tipe function comparator
+ * 
+ * @param arr array yang akan di sort
+ * @param cmp callback function untuk menentukan aturan sorting
+ */
+template<typename T, int N, typename Compare>
+void bubbleSort(T arr[N], Compare cmp) {
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = 0; j < N - i - 1; j++)
+        {
+            if (cmp(arr[j], arr[j+1])) {
+                swap(arr[j], arr[j+1]);
+            }
+        }        
+    }
+}
