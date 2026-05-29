98c69531b2bb91d43f871fb123c9d518374a5057
abcdavk <xloot.mcpedl@gmail.com>
2026-05-29 14:50:05 +0700

    create initSetting()

--- src/setting.cpp
+++ src/setting.cpp
@@ -2,3 +2,9 @@
 #include "setting.h"
 
 Setting setting;
+
+void initSetting() {
+    setting.loadedFile = "database";
+    setting.sortingAlgorithm = SortAlg::Bubble;
+    setting.searchAlgorithm = SearchAlg::Selection;
+}
