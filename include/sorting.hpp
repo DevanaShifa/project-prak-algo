d8b81b6a9640ecb86d86db0043800b280c4079e5
abcdavk <xloot.mcpedl@gmail.com>
2026-05-29 19:05:22 +0700

    arr param is now using reference

--- include/sorting.hpp
+++ include/sorting.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 /**
  * @tparam T tipe data elemen array
@@ -9,13 +10,13 @@
  * @param cmp callback function untuk menentukan aturan sorting
  */
 template<typename T, int N, typename Compare>
-void bubbleSort(T arr[N], Compare cmp) {
+void bubbleSort(T (&arr)[N], Compare cmp) {
     for (int i = 0; i < N - 1; i++)
     {
         for (int j = 0; j < N - i - 1; j++)
         {
             if (cmp(arr[j], arr[j+1])) {
-                swap(arr[j], arr[j+1]);
+                std::swap(arr[j], arr[j+1]);
             }
         }        
     }
